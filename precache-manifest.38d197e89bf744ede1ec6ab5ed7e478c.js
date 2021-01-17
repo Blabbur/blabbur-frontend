3bad4c681499aef32a0bf3537c3fb35b78ae599a
Faiz Surani <faiz.surani@gmail.com>
2021-01-16 17:21:43 -0800

    Updates

---
+++ precache-manifest.38d197e89bf744ede1ec6ab5ed7e478c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d38902b9a974e5b9628b1b990dc95ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ecffad864c18fdadc1d",
+    "url": "/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "2ecffad864c18fdadc1d",
+    "url": "/static/js/2.26ea9982.chunk.js"
+  },
+  {
+    "revision": "6b335196764d0fb87239de7234f0dc7c",
+    "url": "/static/js/2.26ea9982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d65979fd2b29e22c719",
+    "url": "/static/js/main.2d84ebf1.chunk.js"
+  },
+  {
+    "revision": "99dd8f17132002e5ea50",
+    "url": "/static/js/runtime-main.1f5a9929.js"
+  }
+]);
